ab2e8656937933673c462e3023801cbc76e3d13d
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-02-15 17:39:28 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,29 +1,30 @@
 class Translation(object):
     START_TEXT = """Hii <b>{message.from_user.first_name}</b>,
 I am All In One Bot created by @pyrogrammers, i can do a lot of stuffs.
+
 Hit /help for more details..
 """
     IFLONG_FILE_NAME = " Only 64 characters can be named . "
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>Join @pyrogrammers channels to upgrade your plan for free. </b>"
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>File size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail.
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>File size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_FILE = " ⏳Downloading File "
-    UPLOAD_FILE = " ⏳Uploading \n\n To  transfer.sh "
-    ANNO_UPLOAD = " ⏳Uploading \n\n To  anonfiles.com "
-    BAY_UPLOAD = " ⏳Uploading \n\n To  bayfiles.com "
-    GO_FILE_UPLOAD = " ⏳Uploading \n\n To  gofile.io "
-    DOWNLOAD_START = " ⏳Downloading \n\nWait untill it completed."
-    UPLOAD_START = " ⏳Uploading"
+    DOWNLOAD_FILE = " ⏳ Downloading File "
+    UPLOAD_FILE = " ⏳ Uploading \n\n To  transfer.sh "
+    ANNO_UPLOAD = " ⏳ Uploading \n\n To  anonfiles.com "
+    BAY_UPLOAD = " ⏳ Uploading \n\n To  bayfiles.com "
+    GO_FILE_UPLOAD = " ⏳ Uploading \n\n To  gofile.io "
+    DOWNLOAD_START = " ⏳ Downloading \n\nWait⏳ untill it completed."
+    UPLOAD_START = " ⏳ Uploading "
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<a href='https://youtube.com/channel/UC2anvk7MNeNzJ6B4c0SZepw'>Subscribe YouTube</a> "
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = " JOIN : https://t.me/pyrogrammers"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nJoin : @pyrogrammers\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/pyrogrammers'>Pyrogrammers</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
@@ -36,7 +37,6 @@ URL | filename | username | password"""
     CURENT_PLAN_DETAILS = """About you...
 --------
 Telegram ID : <code>{}</code>
-
 """
     HELP_USER = """Hii I am <b>All in one bot</b> and I can perform many tasks.
     
@@ -53,8 +53,8 @@ Telegram ID : <code>{}</code>
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /ren with custom thumbnail support"
     AFTER_GET_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>"
     AFTER_GET_DL_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\nValid for <b>14</b> days."
-    #AFTER_GET_DL_LINK = " {} valid for 30 or more days.\n\n Join : @codexmania \n For the list of Telegram bots. "
-    AFTER_GET_GOFILE_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n<b>File MD5 Checksum :</b> <code>{}</code>\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\n Valid untill 10 days of inactivity."
+    #AFTER_GET_DL_LINK = " {} valid for 30 or more days."
+    AFTER_GET_GOFILE_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n<b>File MD5 Checksum :</b> <code>{}</code>\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\n Valid untill 10 days of inactivity\n"
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS for screenshot of that specific time."""
     FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
@@ -65,11 +65,11 @@ Telegram ID : <code>{}</code>
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
     INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Join : @codexmaniachat \n For talking with the owner. "
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Join : @pyrogrammers \n to help developer."
     EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
     EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
     UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file.Please Report in support Group."
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/programmers'>@pyrogrammers</a>"
     EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
 You can use /rename command after receiving file to rename it with custom thumbnail support."""
     CANCEL_STR = "Process Cancelled"
@@ -77,4 +77,4 @@ You can use /rename command after receiving file to rename it with custom thumbn
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> @transload  and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
